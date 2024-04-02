25c671436e1dca5aee4aed5857c5d4e4ad879cd9
EnikoKiraly <tomekrelski@gmail.com>
2024-04-02 21:35:02 +0200

    Homepage get Sports

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+require('dotenv').config(); // This line loads environment variables from .env file
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+
+app.get('/', async (req, res) => {
+    try {
+        const sports = 'https://api.hubspot.com/crm/v3/objects/sports?limit=10&properties=name,type,equipment&archived=false';
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        };
+        const response = await axios.get(sports, { headers });
+        const sportsData = response.data.results;
+        res.render('homepage', { title: 'Homepage Sports | HubSpot APIs', sportsData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
