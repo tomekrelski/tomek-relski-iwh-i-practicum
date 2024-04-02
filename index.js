99661b941b9b52b8d2852b76a63ca30a5c5284dc
EnikoKiraly <tomekrelski@gmail.com>
2024-04-02 21:45:58 +0200

    updates get

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ app.get('/', async (req, res) => {
 
 // * Code for Route 2 goes here
 
+app.get('/update-cobj', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        res.render('updates', { title: "Update Sports" });
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
